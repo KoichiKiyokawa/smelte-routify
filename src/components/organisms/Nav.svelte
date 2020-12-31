094419d155fbf578729315d5b51f330d31cf7319
K.Kiyokawa <koichi20110068@gmail.com>
2020-12-31 18:21:30 +0900

    chore: absolute path

---
+++ src/components/organisms/Nav.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import AppBar from 'smelte/src/components/AppBar'
+</script>
+
+<AppBar>
+  <h1>Smelte App</h1>
+</AppBar>
---
+++ src/utils/foo.ts
@@ -0,0 +1,3 @@
+import { bar } from 'src/utils/bar'
+
+export const foo = () => console.log(bar)
